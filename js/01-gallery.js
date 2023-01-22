c86d85024b940b58ec098ac4d649b919563328b0
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-01-22 18:24:18 +0200

    task-7/1 start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// const renderItems = (pictures) =>
+//   pictures
+//     .map(
+//       (picture) => `<div class="gallery__item">
+//     <a class="gallery__link" href="large-image.jpg">
+//       <img
+//         class="gallery__image"
+//         src="small-image.jpg"
+//         data-source="large-image.jpg"
+//         alt="Image description"
+//       />
+//     </a>
+//   </div>`
+//     )
+//     .join("");
+console.log();
+const renderGalleryItems = (pictures) =>
+  pictures.reduce(
+    (acc, picture) =>
+      acc +
+      `<div class="gallery__item">
+  <a class="gallery__link" href=${galleryItems.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItems.preview}
+      data-source=${galleryItems.original}
+      alt=${galleryItems.description}
+    />
+  </a>
+</div>`,
+    ""
+  );
+  console.log(galleryItems.original);
+console.log(renderGalleryItems(galleryItems));
+
+const insertGalleryItems = (string) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.insertAdjacentHTML("beforeend", string);
+};
+
+insertGalleryItems(renderGalleryItems(galleryItems));
+
